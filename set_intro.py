a5ab72e6d996c92071885a72d7e8976f2176359e
ebi63 <ebi.mohamadi63@yahoo.com>
2024-10-20 00:12:32 -0700

    pursing course

---
+++ set_intro.py
@@ -0,0 +1,13 @@
+#set.add('')
+#set.remove('')
+#set.discard('')
+#a.intersection(b) or a&b
+#a.union(a) or a|b
+#a.difference(b) or a - b
+#a.symmetric_difference(b) or a^b means delta
+"""
+a = set("salam")
+b = set("varam")
+{x for x in a if x not in b}
+{'s', 'l'}
+"""
